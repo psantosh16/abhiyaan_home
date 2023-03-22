5c73289f0e0232b8441cc9e5a4142962b7204c3e
Omkar Date <omkardate29@gmail.com>
2023-03-22 21:49:11 +0530

    images issue resolved

--- src/Gallery/src/models/Years.jsx
+++ src/Gallery/src/models/Years.jsx
@@ -1,9 +1,9 @@
 const Years = [
-  { 0: "src/Gallery/public/photo-1.avif" },
-  { 1: "src/Gallery/public/photo-2.avif" },
-  { 2: "src/Gallery/public/photo-3.avif" },
-  { 3: "src/Gallery/public/photo-4.avif" },
-  { 4: "src/Gallery/public/photo-5.avif" },
+  { 0: "/public/Gallery-images/photo-1.avif" },
+  { 1: "/public/Gallery-images/photo-2.avif" },
+  { 2: "/public/Gallery-images/photo-3.avif" },
+  { 3: "/public/Gallery-images/photo-4.avif" },
+  { 4: "/public/Gallery-images/photo-5.avif" },
 ];
 
 export default Years;
