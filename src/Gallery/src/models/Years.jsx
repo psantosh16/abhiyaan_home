aaa557902fee59f26ee16dbaa22a3580a82f732e
Omkar Date <omkardate29@gmail.com>
2023-03-22 21:54:28 +0530

    .

--- src/Gallery/src/models/Years.jsx
+++ src/Gallery/src/models/Years.jsx
@@ -1,9 +1,9 @@
 const Years = [
-  { 0: "/public/Gallery-images/photo-1.avif" },
-  { 1: "/public/Gallery-images/photo-2.avif" },
-  { 2: "/public/Gallery-images/photo-3.avif" },
-  { 3: "/public/Gallery-images/photo-4.avif" },
-  { 4: "/public/Gallery-images/photo-5.avif" },
+  { 0: "/Gallery-images/photo-1.avif" },
+  { 1: "/Gallery-images/photo-2.avif" },
+  { 2: "/Gallery-images/photo-3.avif" },
+  { 3: "/Gallery-images/photo-4.avif" },
+  { 4: "/Gallery-images/photo-5.avif" },
 ];
 
 export default Years;
