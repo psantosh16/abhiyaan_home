852ca9bdd5f528c8bcdd71afab337b3f2ea86e69
Niranjan Dorage <niranjandorage@gmail.com>
2023-04-05 10:43:45 +0530

    student council data added

---
+++ src/Studentcouncil/profilecard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Profilecard(props) {
+    return (
+        <div className="profile-card">
+            <div className="img">
+                <img alt="" src={props.img} />
+                <p className="nam">{props.name}</p>
+            </div>
+            <div className="caption">
+                <h3>{props.name}</h3>
+                <p>{props.position}</p>
+                <div className="social-links">
+                    <a href={props.insta}>
+
+                        <i className="fab fa-instagram"></i>
+                    </a>
+                    <a href={props.linked}>
+
+                        <i className="fab fa-linkedin"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profilecard;
