255983113a6e2db20ee02b7d9576a9b053644f7b
Niranjan Dorage <niranjandorage@gmail.com>
2023-04-09 23:22:07 +0530

    minor changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,15 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-     screens: {
-    'zero': '0px',
-    // => @media (min-width: 640px) { ... } 
-
-    'phone': '410px',
-    // => @media (min-width: 768px) { ... }
-
-    
-  },
+     
     extend: {
       fontFamily:{
         poppins:['Poppins', 'sans-serif'],
